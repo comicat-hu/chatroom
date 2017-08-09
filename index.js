06f584b4fe88b674561a1f24881b28e0b87299be
jimmy.huang <jimmy.huang@jimmyhuangde-MacBook-Pro.local>
2017-08-09 13:51:43 +0800

    remove extra space\n\t and add htmlencode

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var socketio = require('socket.io');
 var http = require('http');
 var ejs = require('ejs');
 var config = require('./config.json');
+const htmlEncode = require('htmlencode').htmlEncode
 
 var app = express();
 
@@ -23,30 +24,32 @@ var io = socketio(server);
 server.listen(config.port, () => {
     console.log('Run server at ' + config.host + ':' + config.port + ' - ' + new Date());
 });
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
 
     console.log(socket.id + ' connect ' + new Date().toLocaleString());
 
-    socket.on('clientTest', function(msg){
+    socket.on('clientTest', function (msg) {
         console.log(socket.id + ' ' + msg)
         socket.emit('testOK', '200 OK');
     });
 
-    socket.on('message', function(msg){
+    socket.on('message', function (msg) {
         console.log(socket.id + ' send: ' + msg);
 
         io.emit('broadcast', socket.id + ': ' + htmlCharConvert(msg));
     });
 
-    socket.on('disconnect', function() {
+    socket.on('disconnect', function () {
         console.log(socket.id + ' disconnect' + new Date().toLocaleString());
     });
 });
 
-function htmlCharConvert(str){
-    return str.replace(/&/g, "&amp;")
-            .replace(/>/g, "&gt;")
-            .replace(/</g, "&lt;")
-            .replace(/"/g, "&quot;")
-            .replace(/'/g, "&#039;");
+function htmlCharConvert(str) {
+    str = remove(str, [' ', '\n', '\t'])
+    return htmlEncode(str)
+}
+
+function remove(str, keys) {
+    keys.map(key => str = str.split(key).filter(x => x !== '').join(key))
+    return str
 }
